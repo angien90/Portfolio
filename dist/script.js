76321905dd79660d0847e30dca69d8f35fcca51b
angien90 <angelicanylander@hotmail.com>
2025-08-05 23:25:27 +0200

    Fixa till Typescript

--- dist/script.js
+++ dist/script.js
@@ -1,55 +1,52 @@
+"use strict";
 document.addEventListener("DOMContentLoaded", function () {
     AOS.init({
-      duration: 800,
-      easing: 'slide'
+        duration: 800,
+        easing: 'slide'
     });
-  });
-
+});
 // Dark / light mode
 document.addEventListener('DOMContentLoaded', () => {
     const checkbox = document.getElementById('checkbox');
     const body = document.body;
-
-    // Kolla om det finns en tidigare inställning i localStorage
+    if (!checkbox)
+        return;
     if (localStorage.getItem('theme') === 'dark') {
         checkbox.checked = true;
         body.classList.add('dark-mode');
-    } else {
+    }
+    else {
         checkbox.checked = false;
         body.classList.add('light-mode');
     }
-
     checkbox.addEventListener('change', () => {
         if (checkbox.checked) {
             body.classList.remove('light-mode');
             body.classList.add('dark-mode');
-            localStorage.setItem('theme', 'dark'); // Spara i localStorage
-        } else {
+            localStorage.setItem('theme', 'dark');
+        }
+        else {
             body.classList.remove('dark-mode');
             body.classList.add('light-mode');
-            localStorage.setItem('theme', 'light'); // Spara i localStorage
+            localStorage.setItem('theme', 'light');
         }
     });
 });
-
 // Toggla navigeringstyp beroende på skärm
-  const hamburgerBtn = document.getElementById("hamburger-btn");
-  const navLinks = document.querySelector(".nav-links");
-  const navLinkItems = document.querySelectorAll(".nav-links a");
-
-
-  if (hamburgerBtn && navLinks) {
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const navLinks = document.querySelector(".nav-links");
+const navLinkItems = document.querySelectorAll(".nav-links a");
+if (hamburgerBtn && navLinks) {
     hamburgerBtn.addEventListener("click", function () {
-      navLinks.classList.toggle("active");
-      this.classList.toggle("active");
-      this.textContent = this.classList.contains("active") ? "✖" : "☰";
+        navLinks.classList.toggle("active");
+        hamburgerBtn.classList.toggle("active");
+        hamburgerBtn.textContent = hamburgerBtn.classList.contains("active") ? "✖" : "☰";
     });
-
     navLinkItems.forEach((link) => {
-    link.addEventListener("click", function () {
-      navLinks.classList.remove("active");
-      hamburgerBtn.classList.remove("active");
-      hamburgerBtn.textContent = "☰";
+        link.addEventListener("click", function () {
+            navLinks.classList.remove("active");
+            hamburgerBtn.classList.remove("active");
+            hamburgerBtn.textContent = "☰";
+        });
     });
-  });
-  }
+}
